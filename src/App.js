411d73ac994358d62aae430c6270da441dc10af1
mrami8 <kram20008@gmail.com>
2026-04-08 00:57:52 +0200

    1ER CAMBIO

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
+import Panel from './pages/Panel';
+import Torneos from './pages/Torneos';
+import NuevoTorneo from './pages/NuevoTorneo';
+import Jugadores from './pages/Jugadores';
+import Decklists from './pages/Decklists';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/panel" element={<Panel />} />
+        <Route path="/torneos" element={<Torneos />} />
+        <Route path="/nuevo-torneo" element={<NuevoTorneo />} />
+        <Route path="/jugadores" element={<Jugadores />} />
+        <Route path="/decklists" element={<Decklists />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
